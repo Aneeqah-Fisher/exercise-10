1b0fdc9e5db2f8b1250f6fcfa60f4021741efdb8
Aneeqah <st10454674@rrconnect.edu.za>
2024-08-05 10:40:42 +0200

    first

---
+++ src/App.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class App {
+    // Method to find the index of the smallest element in an array of doubles
+    public static int indexOfSmallestElement(double[] array) {
+        int minIndex = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[minIndex]) {
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double[] numbers = new double[10];
+
+        // Read ten numbers from the user
+        System.out.println("Enter ten numbers:");
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = input.nextDouble();
+        }
+
+        // Display the index of the smallest element
+        System.out.println("The index of the smallest element is: " + indexOfSmallestElement(numbers));
+
+        input.close();
+    }
+}
